0b1fe9e489c48379f9b8fb4c06b196eb216e11ca
praty474 <p.kayastha.pk@gmail.com>
2024-03-05 20:56:59 +0545

    message loading

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -9,45 +11,47 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
+    return MaterialApp(
+      home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Padding(padding: EdgeInsets.all(10)),
+              const CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 40,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextButton.icon(
+                style: const ButtonStyle(
+                  alignment: Alignment.topLeft,
+                ),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                label: const Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text('Chat'),
+        ),
+        body: Column(
           children: [
-            Padding(padding: EdgeInsets.all(10)),
-            const CircleAvatar(
-              backgroundColor: Colors.grey,
-              radius: 40,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextButton.icon(
-              style: const ButtonStyle(
-                alignment: Alignment.topLeft,
-              ),
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              label: const Text(
-                'Logout',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
+            Expanded(child: ChatMessages()),
+            NewMessage(),
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: const Text('Chat'),
-      ),
-      body: Column(
-        children: const [
-          ChatMessages(),
-          ChatScreen(),
-        ],
       ),
     );
   }
